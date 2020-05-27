296b1c8dfb09bf5eda4b9f669aa2aac003db63bb
Marta Adell <marta.adell.noguera@gmail.com>
2020-05-27 22:47:57 +0200

    First test getting info from API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  
+  state={imageURL: ""};
+
+  async componentDidMount() {
+    const res = await axios.get("http://acnhapi.com/v1/villagers");
+    
+    console.log(Object.values(res.data)); //per fer un array per manegar-ho millor (no sé si ho faré servir)
+    this.setState({imageURL: res.data.ant00.icon_uri});
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Hola</h1>
+        {this.state.imageURL !== "" ? <img src={this.state.imageURL}></img>: null}
+      </div>
+    )
+  }
 }
 
-export default App;
